19aa132da0cda4dee9278f28d791ceda20656f2d
kflaoyidz <77423738+Siwat-2402@users.noreply.github.com>
2021-04-24 21:52:30 +0700

    Update Account.vue

--- atrip_project/src/views/Account.vue
+++ atrip_project/src/views/Account.vue
@@ -2,18 +2,17 @@
   <v-content>
     <TripBar />
     <div class="Account">
-      <!-- <h1>This is Account page</h1> -->
       <v-row>
         <v-col cols="3">
-          <v-card height="600" class="account_prf">
+          <v-card class="account_prf test">
             <v-row justify="center">
               <v-avatar class="ma-10" color="primary" size="150"></v-avatar>
             </v-row>
             <v-row justify="center">
-              <v-btn flat class="Btn1"> test btn1</v-btn>
+              <v-btn flat class="Btn1">ทริปของคุณ</v-btn>
             </v-row>
             <v-row justify="center">
-              <v-btn flat class="Btn2"> test btn2</v-btn>
+              <v-btn flat class="Btn2">แก้ไขข้อมูลส่วนตัว</v-btn>
             </v-row>
           </v-card>
         </v-col>
@@ -21,6 +20,23 @@
           <v-card color="orange lighten-4" class="saveTrip">
             <h1 class="saveTxt">Saved Trip</h1>
           </v-card>
+          <v-col cols="6">
+            <v-slide-group
+              v-model="model"
+              class="pa-9"
+              active-class="success"
+              show-arrows
+            >
+              <v-slide-item v-for="n in 15" :key="n">
+                <v-card
+                  :color="active ? undefined : 'grey lighten-1'"
+                  class="group ma-4"
+                  @click="toggle"
+                >
+                </v-card>
+              </v-slide-item>
+            </v-slide-group>
+          </v-col>
         </v-col>
       </v-row>
     </div>
@@ -40,9 +56,19 @@ export default {
 </script>
 
 <style scoped>
+.Account {
+  position: absolute;
+  top: 63px;
+}
 .account_prf {
   margin-left: 40px;
   margin-top: 60px;
+  height: 600px;
+  width: 1300px;
+}
+.group {
+  height: 450px;
+  width: 0px;
 }
 .Btn2 {
   margin-left: 0px;
@@ -54,13 +80,29 @@ export default {
   margin-top: 250px;
   width: 300px;
 }
-.saveTrip{
-  margin-left:70px;
-  margin-top:50px;
-  height:50px;
+.saveTrip {
+  margin-left: 70px;
+  margin-top: 50px;
+  height: 50px;
 }
-.saveTxt{
-  margin-left:30px;
-  margin-top:50px;
+.saveTxt {
+  margin-left: 30px;
+  margin-top: 50px;
 }
+.saveTxt2 {
+  margin-left: 30px;
+  margin-top: 50px;
+}
+/*@media screen and (max-width: 992px) {
+  .account_prf {
+    background-color: blue;
+    color: white;
+  }
+}
+@media screen and (max-width: 600px) {
+  .account_prf {
+    background-color: rgb(80, 229, 255);
+    /* color: white; */
+  /* } */
+/* } */
 </style>
