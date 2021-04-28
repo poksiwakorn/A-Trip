6cd39aece74e8f05361bc7ab0a41385957bee8e8
ryuinw123 <romtham.thang@gmail.com>
2021-04-29 00:36:12 +0700

    V api update

--- Backend/API.py
+++ Backend/API.py
@@ -162,8 +162,7 @@ def getPlace():
         print(content)
         print(len(content["place"]))
         if content["place"]:
-            contentinput = content["place"][1:len(content["place"])-1]
-            contentinput = contentinput.split(",")
+            contentinput = content["place"][1:len(content["place"])-1].split(",")
             print(contentinput)
             form = "SELECT * FROM Atrip_Place1 WHERE keyID = " + " or keyID = ".join(contentinput)
             print(form)
